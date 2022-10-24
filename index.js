5431857bcd9b9b36933f1375cb28b32efbb2950b
Henry <lihenryhl@yahoo.com>
2022-10-24 13:13:32 -0700

    testing

--- index.js
+++ index.js
@@ -80,6 +80,7 @@ app.get("/verify/:token", async (req, res)=>{
 app.post("/tester", async (req,res) => {
     console.log(req.body)
     const {username, password, email} = req.body
+    res.status(200).send({user:username, password:password, email:email})
 })
 
 app.post("/createUser", async (req,res) => {
