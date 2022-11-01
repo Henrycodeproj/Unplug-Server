feddbdf58c2b450587b3f869e8de999ce26ad526
Henry <lihenryhl@yahoo.com>
2022-11-01 01:05:48 -0700

    reverting expiry

--- Models/Posts.js
+++ Models/Posts.js
@@ -13,7 +13,7 @@ const PostSchema = new mongoose.Schema({
     expiresAt: {
         type: Date,
         default: Date.now,
-        index: { expires: '3d' }
+        expires: '3d',
     }
 },{timestamps:true})
 
