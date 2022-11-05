691ec681c66a257e29f79b915ea0488a17018d80
Henry <lihenryhl@yahoo.com>
2022-11-05 03:36:05 -0700

    test

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ let activeUsers = {}
 io.on("connection", (socket) => {
 
     socket.on("status", (userInfo) => {
-        console.log(!userInfo.userId in activeUsers, userInfo)
+        console.log(userInfo.userId, userInfo.userId)
         if (!userInfo.userId in activeUsers){
             activeUsers[userInfo.userId] = socket.id
             socket[socket.id] = userInfo.userId
