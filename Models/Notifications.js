8cdb15ff917d51a020159510322552c209522b19
Henry <lihenryhl@yahoo.com>
2022-12-21 02:27:21 -0800

    changing notification expiration

--- Models/Notifications.js
+++ Models/Notifications.js
@@ -16,7 +16,7 @@ const NotificationSchema =  new mongoose.Schema({
     expiresAt: {
         type: Date,
         default: Date.now,
-        expires: 60,
+        expires: '2d',
     }
 }, {timestamps:true})
 
